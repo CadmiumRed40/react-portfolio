e3466c40bf27aef14331ce563161dd4fdbb4e597
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-03-08 23:19:25 -0500

    fine tuning glossy look on components

--- src/components/Welcomebox/Welcomebox.js
+++ src/components/Welcomebox/Welcomebox.js
@@ -4,12 +4,16 @@ import '../Welcomebox/Welcomebox.css'
 export default function Welcomebox() {
   return (
     <div>
+        <div className='main-container'>
         <header className="header">
             <div className="header-content">
                 <h1 className="header-title">Hello, my name is Enrique.</h1>
                     <span className="header-subtitle">And I am your future software engineer.</span>
             </div>
-    </header>
+        </header>
+
+        <img src='/Users/bluecobalt/Documents/Coding/REACTAPPS/react-portfolio-website/src/components/Welcomebox/IMG_20170223_155522_148.jpeg' alt='profile-pic'></img>
+        </div>
     </div>
   )
 }
