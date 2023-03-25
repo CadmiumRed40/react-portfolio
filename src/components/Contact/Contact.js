5a2a08d1a1beccdda74d92f3f1c993b70bdee522
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-03-25 16:43:19 -0400

    created contact component and logic

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Contact() {
-  return (
-    <div>Contact</div>
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleNameChange = event => setName(event.target.value);
+    const handleEmailChange = event => setEmail(event.target.value);
+    const handleMessageChange = event => setMessage(event.target.value);
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
+
+    return(
+
+    <div>
+        <div className='contact-container'>
+            <form onSubmit={handleSubmit}>
+                
+                <label>
+                    Name:
+                    <input type='text' value={name} onChange={handleNameChange} />
+                </label>
+
+                <label>
+                    Email:
+                    <input type='email' value={email} onChange={handleEmailChange} />
+                </label>
+
+                <label>
+                    Message:
+                    <textarea value={message} onChange={handleMessageChange} />
+                </label>
+                
+                <button type='submit'>submit</button>
+            </form>
+        </div>
+    </div>
   )
 }
