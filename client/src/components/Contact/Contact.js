8a9b32d51a4d408d0df48ab8c58c8ee8c114257a
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-04-03 23:45:13 -0400

    setting contact info to localstorage. Need to change email service as google doesnt allow third party applications direct access to email accounts.

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function ContactForm() {
@@ -7,6 +7,7 @@ export default function ContactForm() {
   const [message, setMessage] = useState('');
   const [isSent, setIsSent] = useState(false); //
 
+//handles the submitting portion of the component. We create a post request of the object specified to the localhost address
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.post('http://localhost:3001/send-email', { name, email, message })
@@ -15,24 +16,38 @@ export default function ContactForm() {
         setName('');
         setEmail('');
         setMessage('');
-        setIsSent(true)
+        setIsSent(true);
+        localStorage.setItem('name', name);
+        localStorage.setItem('email', email)
       })
       .catch((err) => {
         console.error(err);
       });
   };
 
+  //displaying the object from localstorage back when the component mounts using useEffect and no dependency array to only run on the initial render.
+
+  useEffect(() => {
+    const storedName = localStorage.getItem('name');
+    const storedEmail = localStorage.getItem('email');
+
+    if (storedName && storedEmail) {
+      setName(storedName);
+      setEmail(storedEmail);
+    }
+  },[]);
+
   return (
     <form onSubmit={handleSubmit}>
-      <label>
+      <label className='form-headers'>
         Name:
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </label>
-      <label>
+      <label className='form-headers'>
         Email:
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
       </label>
-      <label>
+      <label className='form-headers'>
         Message:
         <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
       </label>
